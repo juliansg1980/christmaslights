b64d7cdde075e4d1d301db08dbcd3f181234d31e
Julian Sanz <juliansg1980@hotmail.com>
2021-11-11 12:37:44 +0100

    refactor extract field

--- src/test/java/com/thoughtworks/katas/christmaslights/PanelShould.java
+++ src/test/java/com/thoughtworks/katas/christmaslights/PanelShould.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.katas.christmaslights;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,6 +10,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PanelShould {
+
+    private TestPanel panel;
+
+    @BeforeEach
+    public void SetUp() {
+        panel = new TestPanel();
+    }
+
     @ParameterizedTest
     @CsvSource({"0,0", "999,0", "0, 999", "999, 999"})
     public void turn_on_a_light(int xCoordinate, int yCoordinate) {
@@ -21,7 +30,6 @@ public class PanelShould {
 
     @Test
     public void turn_off_a_light() {
-        TestPanel panel = new TestPanel();
         panel.getLights()[0][0] = true;
 
         panel.turnOff();
