fb62bbac427f54a670efe19605c359e0cae5bf72
Julian Sanz <juliansg1980@hotmail.com>
2021-11-11 12:07:23 +0100

    refactor optimize imports

--- src/test/java/com/thoughtworks/katas/christmaslights/PanelShould.java
+++ src/test/java/com/thoughtworks/katas/christmaslights/PanelShould.java
@@ -2,7 +2,6 @@ package com.thoughtworks.katas.christmaslights;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class PanelShould {
