01c63e63b1577a957cd7333e44b5aab3b172e0aa
Julian Sanz <juliansg1980@hotmail.com>
2021-11-11 13:57:53 +0100

    refactor extract method

--- src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
+++ src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
@@ -18,9 +18,13 @@ public class Panel {
     }
 
     public void toggle(int xCoordinate, int yCoordinate) {
-        if (lights[0][0])
+        if (isLightOn())
             turnOff(0, 0);
         else
             turnOn(xCoordinate, yCoordinate);
+    }
+
+    private boolean isLightOn() {
+        return lights[0][0] == true;
     }
 }
