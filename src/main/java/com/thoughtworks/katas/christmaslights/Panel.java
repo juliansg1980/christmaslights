37b696b28fd6b13a1e5b8334b4c8ee122a4407be
Julian Sanz <juliansg1980@hotmail.com>
2021-11-11 12:20:02 +0100

    add test case

--- src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
+++ src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
@@ -2,14 +2,14 @@ package com.thoughtworks.katas.christmaslights;
 
 public class Panel {
     public static final int xLength = 1000;
-    public static final int yLength = 1;
+    public static final int yLength = 1000;
     protected boolean lights[][];
 
     public Panel() {
         lights = new boolean[xLength][yLength];
     }
 
-    public void turnOn(int xCoordinate) {
-        lights[xCoordinate][0] = true;
+    public void turnOn(int xCoordinate, int yCoordinate) {
+        lights[xCoordinate][yCoordinate] = true;
     }
 }
