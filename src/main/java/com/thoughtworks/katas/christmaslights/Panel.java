6de65742f966a71cb3601b6677021fa370eb698c
Julian Sanz <juliansg1980@hotmail.com>
2021-11-11 12:08:17 +0100

    refactor extract constants

--- src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
+++ src/main/java/com/thoughtworks/katas/christmaslights/Panel.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.katas.christmaslights;
 
 public class Panel {
+    public static final int xLength = 1;
+    public static final int yLength = 1;
     protected boolean lights[][];
 
     public Panel() {
-        lights = new boolean[1][1];
+        lights = new boolean[xLength][yLength];
     }
 
     public void turnOn() {
